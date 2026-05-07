85b3a64454becf83bf2a19261a40366ad5e2d160
Quentin <quentin.joneaux@etu.univ-st-etienne.fr>
2026-05-07 11:50:30 +0200

    Test du déploiement automatique

--- mon-kaban/src/App.jsx
+++ mon-kaban/src/App.jsx
@@ -1,11 +1,11 @@
 function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center' }}>
-      <h1> Mon premier déploiement</h1>
+      <h1>Déploiement automatique en direct</h1>
       <p>Projet réalisé par <strong>Quentin JONEAUX</strong></p>
       <p>Future application : gestion de tâches Kanban</p>
     </div>
   );
 }
 
-export default App;
+export default App; 
